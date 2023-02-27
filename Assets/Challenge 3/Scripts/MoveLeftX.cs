a73ac802e9ecde31ce4d29827b9d1251c6d7435f
Maria20Maria20 <mari.avi00@mail.ru>
2023-02-27 10:01:06 +0400

    1 version

---
+++ Assets/Challenge 3/Scripts/MoveLeftX.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLeftX : MonoBehaviour
+{
+    public float speed;
+    private PlayerControllerX playerControllerScript;
+    private float leftBound = -10;
+
+    
+    void Start()
+    {
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerControllerX>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Если игра не проиграна, то двигаем объект влево
+        if (!playerControllerScript.gameOver)
+        {
+            transform.Translate(Vector3.left * speed * Time.deltaTime, Space.World);
+        }
+
+        // Если объект ушел за пределы экрана и этот объект не фон, то УНИЧТОЖИТЬ его
+        if (transform.position.x < leftBound && !gameObject.CompareTag("Background"))
+        {
+            Destroy(gameObject);
+        }
+
+    }
+}
---
+++ Assets/Challenge 3/Scripts/RepeatBackgroundX.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepeatBackgroundX : MonoBehaviour
+{
+    private Vector3 startPos;
+    private float repeatWidth;
+
+    private void Start()
+    {
+        startPos = transform.position; // Телепортация в стартовую позицию
+        repeatWidth = GetComponent<BoxCollider>().size.x / 2; // Высчитываем центр фона
+    }
+
+    private void Update()
+    {
+        // Если фон ушёл на половину влево, то возвращаем его на стартовую позицию
+        if (transform.position.x < startPos.x - repeatWidth)
+        {
+            transform.position = startPos;
+        }
+    }
+
+ 
+}
+
+
